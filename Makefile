3bcfbe02dfbdc10074d6d11afcfebc2f650a20f5
pashamray <pashamray@gmail.com>
2016-04-06 21:38:19 +0300

    add buttons add states add change temp

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ PROJECT_DIR   = .
 ARDMK_DIR = $(PROJECT_DIR)/Arduino-Makefile
 ARDUINO_DIR = c:/Arduino
 USER_LIB_PATH := $(PROJECT_DIR)/lib
-ARDUINO_LIBS = Adafruit-GFX-Library Adafruit_SSD1306 Wire OneWire SPI Arduino-Temperature-Control-Library
+ARDUINO_LIBS = Adafruit-GFX-Library Adafruit_SSD1306 Wire OneWire SPI Arduino-Temperature-Control-Library Button EEPROM
 
 BOARD_TAG = uno
 
--- boiler-termostat.ino
+++ boiler-termostat.ino
@@ -1,12 +1,15 @@
 /*
   Boiler termostat
  */
+#include <Arduino.h>
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <Button.h>
+#include <EEPROM.h>
 
 #define OLED_RESET 4
 #define ONE_WIRE_BUS 9
@@ -15,23 +18,47 @@ Adafruit_SSD1306 oled(OLED_RESET);
 OneWire oneWire(ONE_WIRE_BUS);
 // Pass our oneWire reference to Dallas Temperature.
 DallasTemperature sensors(&oneWire);
-DeviceAddress boilerThermometer;
 
 #define ERR_TEMP 10
-#define MIN_TEMP 60
+#define MIN_TEMP 40
 #define MAX_TEMP 70
 
-// function that will be called when an alarm condition exists during DallasTemperatures::processAlarms();
-void newAlarmHandler(const uint8_t* deviceAddress)
-{
-  float tempC = sensors.getTempC(deviceAddress);
-  oled.print("A: ");
-  oled.print(tempC);
-  oled.display();
-}
+#define BTN_SET  10
+#define BTN_PLUS  11
+#define BTN_MINUS  12
 
+#define DELAY_TEMP 5000
+#define DELAY_DISPLAY 250
+
+Button btnSet = Button(BTN_SET, BUTTON_PULLUP_INTERNAL);
+Button btnPlus = Button(BTN_PLUS, BUTTON_PULLUP_INTERNAL);
+Button btnMinus = Button(BTN_MINUS, BUTTON_PULLUP_INTERNAL);
+
+enum { DEFAULT_STATE, MAX_TEMP_STATE, MIN_TEMP_STATE };
+
+byte state = DEFAULT_STATE;
+byte max_temp_val = MAX_TEMP;
+byte min_temp_val = MIN_TEMP;
+
+int current_time = 0;
+int temp_time = 5000;
+int display_time = 250;
+int tempC;
 
 void setup() {
+
+  max_temp_val = EEPROM.read(0);
+  min_temp_val = EEPROM.read(1);
+
+  if ((max_temp_val < MIN_TEMP) || (max_temp_val > MAX_TEMP))
+  {
+    max_temp_val = MAX_TEMP;
+  }
+
+  if ((min_temp_val < MIN_TEMP) || (min_temp_val > MAX_TEMP))
+  {
+    min_temp_val = MIN_TEMP;
+  }
 
   // by default, we'll generate the high voltage from the 3.3v line internally! (neat!)
   oled.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
@@ -48,36 +75,94 @@ void setup() {
 
   // Start up the library
   sensors.begin();
-
-  // search for devices on the bus and assign based on an index
-  if (!sensors.getAddress(boilerThermometer, 0)) {
-    oled.println("Unable to find address for Device 0");
-    oled.display();
-  }
-
-  sensors.setHighAlarmTemp(boilerThermometer, MIN_TEMP);
-  sensors.setLowAlarmTemp(boilerThermometer, MAX_TEMP);
-
-  // attach alarm handler
-  sensors.setAlarmHandler(&newAlarmHandler);
 }
 
 void loop() {
 
-  sensors.requestTemperatures(); // Send the command to get temperatures
+  current_time = millis();
 
-  float tempC = sensors.getTempCByIndex(0);
+  switch(state)
+  {
+    case DEFAULT_STATE:
+      btnSet.scan();
+      if(btnSet.stateChanged()) {
+        if(btnSet.isPressed()) {
+          state = MAX_TEMP_STATE;
+        }
+      }
+      break;
+    case MAX_TEMP_STATE:
+      btnPlus.scan();
+      if(btnPlus.stateChanged()) {
+        if(btnPlus.isPressed()) {
+          if(max_temp_val < MAX_TEMP) max_temp_val++;
+        }
+      }
 
-  oled.setTextSize(2);
-  oled.setTextColor(WHITE);
-  oled.setCursor(10,0);
-  oled.clearDisplay();
-  oled.println(tempC);
-  oled.display();
+      btnMinus.scan();
+      if(btnMinus.stateChanged()) {
+        if(btnMinus.isPressed()) {
+          if(max_temp_val > MIN_TEMP) max_temp_val--;
+        }
+      }
 
-  // call alarm handler function defined by sensors.setAlarmHandler
-  // for each device reporting an alarm
-  sensors.processAlarms();
+      btnSet.scan();
+      if(btnSet.stateChanged()) {
+        if(btnSet.isPressed()) {
+          EEPROM.write(0, max_temp_val);
+          state = MIN_TEMP_STATE;
+        }
+      }
+      break;
+    case MIN_TEMP_STATE:
 
-  delay(1000);
+      btnPlus.scan();
+      if(btnPlus.stateChanged()) {
+        if(btnPlus.isPressed()) {
+          if(min_temp_val < MAX_TEMP) min_temp_val++;
+        }
+      }
+
+      btnMinus.scan();
+      if(btnMinus.stateChanged()) {
+        if(btnMinus.isPressed()) {
+          if(min_temp_val > MIN_TEMP) min_temp_val--;
+        }
+      }
+
+      btnSet.scan();
+      if(btnSet.stateChanged()) {
+        if(btnSet.isPressed()) {
+          EEPROM.write(1, min_temp_val);
+          state = DEFAULT_STATE;
+        }
+      }
+      break;
+  }
+
+  if(current_time - temp_time >= DELAY_TEMP)
+  {
+    temp_time = current_time;
+    sensors.requestTemperatures(); // Send the command to get temperatures
+    tempC = sensors.getTempCByIndex(0);
+  }
+
+  if(current_time - display_time >= DELAY_DISPLAY)
+  {
+    display_time = current_time;
+
+    oled.clearDisplay();
+    oled.setTextColor(WHITE);
+    oled.setTextSize(1);
+    oled.setCursor(0,0);
+    oled.println(max_temp_val);
+    oled.setCursor(25,5);
+    oled.setTextSize(8);
+    oled.println(tempC);
+    oled.setCursor(0,55);
+    oled.setTextSize(1);
+    oled.println(min_temp_val);
+
+    oled.display();
+  }
 }
